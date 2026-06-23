abd9370ca0b41c43d2559582f1d44f764990368c
Ori Mizrahi <ori.mizrahi1000@gmail.com>
2026-06-24 00:40:37 +0300

    With SIMD

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <immintrin.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,57 +7,46 @@
 #include <math.h>
 
 struct Vec2 {
-  double x;
-  double y;
+  float *x;
+  float *y;
 };
 
 static const int32_t WIDTH = 1280;
 static const int32_t HEIGHT = 1280;
-static const size_t BODIES = 1000;
+static const size_t BODIES = 240;
 static const double MASS = 500000.;
 static const double GRAVITY_CONST = .5;
 static const float RADIUS = 5;
 
 struct Vec2 *accelerations, *velocities, *positions;
 
-struct Vec2 vec2_add(struct Vec2 *vec1, struct Vec2 *vec2) {
-  struct Vec2 vec = {.x = vec1->x + vec2->x, .y = vec1->y + vec2->y};
-  return vec;
-}
-
-struct Vec2 vec2_sub(struct Vec2 *vec1, struct Vec2 *vec2) {
-  struct Vec2 vec = {.x = vec1->x - vec2->x, .y = vec1->y - vec2->y};
-  return vec;
-}
-
-double vec2_len(struct Vec2 *vec) {
-  return sqrt(vec->x * vec->x + vec->y * vec->y);
-}
-
-struct Vec2 vec2_scalar(struct Vec2 *vec, double scalar) {
-  struct Vec2 out = {.x = vec->x * scalar, .y = vec->y * scalar};
-  return out;
-}
-
 int32_t rand_xy(int32_t x, int32_t y) {
   return (rand() % (y - 1)) + x;
 }
 
-double rand_real() {
-  return (double)rand() / RAND_MAX;
+float rand_real() {
+  return (float)rand() / RAND_MAX;
 }
 
 void init(void) {
-  accelerations = (struct Vec2 *)calloc(BODIES, sizeof(struct Vec2));
-  velocities = (struct Vec2 *)malloc(BODIES * sizeof(struct Vec2));
-  positions = (struct Vec2 *)malloc(BODIES * sizeof(struct Vec2));
+  accelerations = (struct Vec2 *)malloc(sizeof(struct Vec2));
+  accelerations->x = (float *)calloc(BODIES, sizeof(float));
+  accelerations->y = (float *)calloc(BODIES, sizeof(float));
+  
+  velocities = (struct Vec2 *)malloc(sizeof(struct Vec2));
+  velocities->x = (float *)malloc(BODIES * sizeof(float));
+  velocities->y = (float *)malloc(BODIES * sizeof(float));
+
+  positions = (struct Vec2 *)malloc(sizeof(struct Vec2));
+  positions->x = (float *)malloc(BODIES * sizeof(float));
+  positions->y = (float *)malloc(BODIES * sizeof(float));
 
   for (size_t i = 0; i < BODIES; ++i) {
-    positions[i].x = rand_xy(10, WIDTH);
-    positions[i].y = rand_xy(10, WIDTH);
+    positions->x[i] = rand_xy(10, WIDTH);
+    positions->y[i] = rand_xy(10, WIDTH);
 
-    velocities[i].x = rand_real();
-    velocities[i].y = rand_real();
+    velocities->x[i] = rand_real();
+    velocities->y[i] = rand_real();
   }
 }
 
@@ -68,36 +58,70 @@ void deinit(void) {
 
 void calculate_accelerations(void) {
   for (size_t i = 0; i < BODIES; ++i) {
-    accelerations[i].x = 0;
-    accelerations[i].y = 0;
+    accelerations->x[i] = 0;
+    accelerations->y[i] = 0;
 
     for (size_t j = 0; j < BODIES; ++j) {
       if (i == j)
         continue;
 
-      struct Vec2 r = vec2_sub(&positions[i], &positions[j]);
-      double len = vec2_len(&r);
-      r = vec2_scalar(&r, 1 / len);
+      float dx = positions->x[i] - positions->x[j];
+      float dy = positions->y[i] - positions->y[j];
+      float len = sqrt(dx * dx + dy * dy);
+      dx /= len;
+      dy /= len;
+      
+      float force = GRAVITY_CONST * (MASS / (len * len));
+      accelerations->x[i] -= force * dx;
+      accelerations->y[i] -= force * dy;
 
-      struct Vec2 added_accel = vec2_scalar(&r, GRAVITY_CONST * (MASS / (len * len)));
-      accelerations[i] = vec2_sub(&accelerations[i], &added_accel);
+    /*   struct Vec2 r = vec2_sub(&positions[i], &positions[j]); */
+    /*   double len = vec2_len(&r); */
+    /*   r = vec2_scalar(&r, 1 / len); */
+
+    /*   struct Vec2 added_accel = vec2_scalar(&r, GRAVITY_CONST * (MASS / (len * len))); */
+    /*   accelerations[i] = vec2_sub(&accelerations[i], &added_accel); */
+    /* } */
+
+
     }
   }
 }
 
 void calculate_velocities(void) {
-  for (size_t i = 0; i < BODIES; ++i) {
-    struct Vec2 added_vel = vec2_scalar(&accelerations[i], GetFrameTime());
+  for (size_t i = 0; i < BODIES; i += 8) {
+    float dt = GetFrameTime();
+    __m256 dts = _mm256_broadcast_ss(&dt);
+    __m256 accel_x = _mm256_loadu_ps(&accelerations->x[i]);
+    __m256 accel_y = _mm256_loadu_ps(&accelerations->y[i]);
 
-    velocities[i] = vec2_add(&velocities[i], &added_vel);
+    __m256 added_accel_x = _mm256_mul_ps(accel_x, dts);
+    __m256 added_accel_y = _mm256_mul_ps(accel_y, dts);
+
+    __m256 vel_x = _mm256_add_ps(_mm256_loadu_ps(&velocities->x[i]), added_accel_x);
+    __m256 vel_y = _mm256_add_ps(_mm256_loadu_ps(&velocities->y[i]), added_accel_y);
+
+    _mm256_storeu_ps(&velocities->x[i], vel_x);
+    _mm256_storeu_ps(&velocities->y[i], vel_y);
   }
 }
 
+// Pretty much same calculation as velocities
 void calculate_positions(void) {
-  for (size_t i = 0; i < BODIES; ++i) {
-    struct Vec2 added_pos = vec2_scalar(&velocities[i], GetFrameTime());
+  for (size_t i = 0; i < BODIES; i += 8) {
+    float dt = GetFrameTime();
+    __m256 dts = _mm256_broadcast_ss(&dt);
+    __m256 vel_x = _mm256_loadu_ps(&velocities->x[i]);
+    __m256 vel_y = _mm256_loadu_ps(&velocities->y[i]);
 
-    positions[i] = vec2_add(&positions[i], &added_pos);
+    __m256 added_vel_x = _mm256_mul_ps(vel_x, dts);
+    __m256 added_vel_y = _mm256_mul_ps(vel_y, dts);
+
+    __m256 pos_x = _mm256_add_ps(_mm256_loadu_ps(&positions->x[i]), added_vel_x);
+    __m256 pos_y = _mm256_add_ps(_mm256_loadu_ps(&positions->y[i]), added_vel_y);
+
+    _mm256_storeu_ps(&positions->x[i], pos_x);
+    _mm256_storeu_ps(&positions->y[i], pos_y);
   }
 }
 
@@ -109,7 +133,7 @@ void simulate(void) {
 
 void draw_bodies(void) {
   for (size_t i = 0; i < BODIES; ++i) {
-    DrawCircle(positions[i].x, positions[i].y, RADIUS, RED);
+    DrawCircle(positions->x[i], positions->y[i], RADIUS, RED);
   }
 }
 
